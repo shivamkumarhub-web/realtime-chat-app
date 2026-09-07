cd18138991f294797bcf499b89b3906409477e41
SHIVAM KUMAR <shivamkumarhub@gmail.com>
2026-09-07 20:40:19 +0530

    feat: main Spring Boot application

---
+++ backend/src/main/java/com/shivam/chat/RealtimeChatApplication.java
@@ -0,0 +1,11 @@
+package com.shivam.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtimeChatApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RealtimeChatApplication.class, args);
+    }
+}
